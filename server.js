db54c738be54bf675c1939b5798a22b2e2bf0ab1
alvarofuente-cl <alvarofuente@gmail.com>
2024-11-25 21:12:44 -0300

    Commit 3

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const dotenv = require('dotenv');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const path = require("path");
 
 const connectDB = require('./db');
 
@@ -16,7 +19,35 @@ app.use('/', routes);
 
 connectDB();
 
+const swaggerOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Proyecto 6",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: `http://localhost:${PORT}`,
+        },
+      ],
+      components: {
+        securitySchemes: {
+          ApiKeyAuth: {
+            type: "apiKey",
+            in: "header",
+            name: "x-auth-token",
+          },
+        },
+      },
+    },
+    apis: [`${path.join(__dirname, "./routes/*.js")}`],
+  };
+  
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 const PORT = process.env.PORT || 5000;
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
